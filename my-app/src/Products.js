5e53d343cc876d5830c26a78c112d8c02bbd7c3e
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-27 10:34:28 -0500

    products

--- my-app/src/Products.js
+++ my-app/src/Products.js
@@ -1,8 +1,22 @@
 import React from "react"
+import productsData from "./productsData"
 
-function Products() {    
+function Products() { 
+    const products = productsData.map(prod => (
+        <div key ={prod.id}>
+            <h3>{prod.name}</h3>
+            <p>Price : ${prod.price}</p>
+            <hr/>
+
+        </div>
+    ))
+
+       
     return (
+        <>
         <h1>Products Page</h1>
+        {products}
+        </>
     )
 }
 
