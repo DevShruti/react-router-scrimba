91e0d9c79a9dc1cb23257b56d0f5dbf525f3d18d
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-27 11:36:41 -0500

    product detail

--- my-app/src/ProductDetail.js
+++ my-app/src/ProductDetail.js
@@ -1,6 +1,9 @@
 import React from "react"
+import {useParams} from "react-router-dom"
 
 function ProductDetail() {
+    const {productId} = useParams()
+    console.log(productId)
     return (
         <h1>Product Detail Page</h1>
     )
