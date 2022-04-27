1833ea7a10fa4290c5e84aa8029d4b606de44734
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-27 11:07:14 -0500

    challenge 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,17 +19,17 @@ function App() {
                   </Route>
             </Routes>
           
-            {/*
-                Route between 2 components depending on the path:
-                1. Home page ("/")
-                2. Products Page ("/products")
-                
-                ** Home Page **
-                • Can be a basic home page, maybe welcoming the user to your site and giving some background about yourself as a developer
-                
-                ** Products Page **
-                • Using an array of data for your products (provided for you), map over that data to show each item and some information on the products page as a list of available products.
-            */}
+            {/**
+ * Challenge:
+ * 
+ * 1. Create a ProductDetail component
+ * 2. Link each product name to a detail page of that product 
+ *    under the route "/products/{insert product id here}" (e.g.: "/products/2")
+ * 3. Clicking the product name should replace the product list page with
+ *    the detail page of that component.
+ * 
+ * Hint: Check out the `useParams` lesson if you need a refresher.
+ */}
             
         </div>
     )
